4e887e72b4882edb9b1e4ea1b56b39a246c26f3f
akhmadjonakbarov <akhmadjonakbarov@gmail.com>
2023-02-17 03:56:16 +0500

    news cubit was done

--- lib/controllers/cubits/news/news_cubit.dart
+++ lib/controllers/cubits/news/news_cubit.dart
@@ -1,10 +1,43 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
-import '../../../models/models.dart';
-
+import 'package:news_app/controllers/api/api.dart';
+import 'package:http/http.dart' as http;
+import '../../../models/models.dart' as model;
 part 'news_state.dart';
 
 class NewsCubit extends Cubit<NewsState> {
   NewsCubit() : super(NewsInitial());
+
+  Future<void> getNews({int? categoryId}) async {
+    List<model.New> news = [];
+    Uri url = Uri.parse(
+      Api.newsQueryUrl(hasVideo: true, categoryId: categoryId),
+    );
+    try {
+      emit(NewsLoading());
+      http.Response response = await http.get(url);
+      var resData = jsonDecode(response.body);
+      for (Map<String, dynamic> _new in resData) {
+        model.New new_ = model.New(
+            id: _new['id'],
+            title: _new['title'],
+            description: _new['description'],
+            images: _new['images'],
+            created_on: _new['created_on'],
+            updated_on: _new['updated_on']);
+        news.add(new_);
+      }
+      emit(
+        NewsLoaded(news: news),
+      );
+    } catch (e) {
+      emit(
+        NewsError(
+          errorMsg: e.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/controllers/cubits/news/news_state.dart
+++ lib/controllers/cubits/news/news_state.dart
@@ -8,6 +8,11 @@ class NewsInitial extends NewsState {}
 class NewsLoading extends NewsState {}
 
 class NewsLoaded extends NewsState {
-  final List<New> images;
-  NewsLoaded({required this.images});
+  final List<model.New> news;
+  NewsLoaded({required this.news});
+}
+
+class NewsError extends NewsState {
+  final String errorMsg;
+  NewsError({required this.errorMsg});
 }
